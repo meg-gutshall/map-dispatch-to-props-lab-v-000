7f0c2b5af12563fdd908cc1cd75c34fc2cff654f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-18 15:45:13 -0400

    Done.

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -1,16 +1,11 @@
-export default function manageRestaurants(state = {
-  restaurants: []
-}, action) {
+export default function manageRestaurants(state = { restaurants: [] }, action) {
   switch (action.type) {
-
     case 'ADD_RESTAURANT':
       return {
         ...state,
         restaurants: [...state.restaurants, action.restaurant]
       }
-
     default:
       return state;
-
   }
 };
